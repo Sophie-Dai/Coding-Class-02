99c3d7fe95c6758d72dd840f44f13fdef61542a0
Sophie-Dai <54985332+Sophie-Dai@users.noreply.github.com>
2019-09-14 14:39:56 +0800

    Try the four operators.

---
+++ try_operator2.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+
+lan = 980
+
+for i in range(5):
+    lan += 10
+
+print(lan)
+
+for i in range(5):
+    lan -= 10
+
+print(lan)
+
+for i in range(5):
+    lan *= 10
+
+print(lan)
+
+for i in range(5):
+    lan /= 10
+
+print(lan)
+
+for i in range(5):
+    lan %= 10
+
+print(lan)
+
+for i in range(5):
+    lan **= 10
+
+print(lan)
+
+for i in range(5):
+    lan //= 10
+
+print(lan)
---
+++ try_operator3.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+# a = False
+
+a = (1>0) and (-1>0)
+
+if not a :
+    print('True')
+else:
+    print('False')
